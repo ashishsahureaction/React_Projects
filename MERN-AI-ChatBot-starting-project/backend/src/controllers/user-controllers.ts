3f733126f19bc9662c08507febb88d76fa37b87b
ashishsahureaction <148655100+ashishsahureaction@users.noreply.github.com>
2024-01-15 15:43:41 -0500

    commit message

---
+++ MERN-AI-ChatBot-starting-project/backend/src/controllers/user-controllers.ts
@@ -0,0 +1,50 @@
+import { NextFunction, Request, Response } from "express";
+import User from "../models/User.js";
+import { hash ,compare} from "bcrypt";
+import { createToken } from "../utils/token-manager.js";
+
+export const getAllUsers = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => {
+    try {
+      //get all users
+      const users = await User.find();
+      return res.status(200).json({ message: "OK", users });
+    } catch (error) {
+      console.log(error);
+      return res.status(200).json({ message: "ERROR", cause: error.message });
+    }
+  };
+
+ 
+
+  export const userSignup = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => {
+    try {
+      //users signup 
+      const { name, email, password } = req.body;
+      const hashedPassword = await hash(password, 10);
+      const user = new User({ name, email, password: hashedPassword });
+      await user.save();
+      return res.status(201).json({ message: "OK", id: user._id.toString() });
+    } catch (error) {
+      console.log(error);
+      return res.status(200).json({ message: "ERROR", cause: error.message });
+    }
+  };
+
+  const token = createToken(user._id.toString(), user.email, "7d");
+  const expires = new Date();
+  expires.setDate(expires.getDate() + 7);
+  res.cookie("auth_token", token, {
+    path: "/",
+    domain: "localhost",
+    expires,
+    httpOnly: true,
+    signed: true,
+  });
